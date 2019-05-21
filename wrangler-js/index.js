c0789cc574a45e47220ec8f1c19af844b555b729
Sven Sauleau <sven@cloudflare.com>
2019-05-21 16:53:41 +0100

    fix: entry as absolute path

--- wrangler-js/index.js
+++ wrangler-js/index.js
@@ -9,7 +9,10 @@ function hasFlag(name) {
 
 let config;
 if (hasFlag("auto-webpack-config") === true) {
-  const entry = require(join(process.cwd(), "package.json")).main;
+  const entry = join(
+    process.cwd(),
+    require(join(process.cwd(), "package.json")).main
+  );
   config = { entry };
 } else {
   config = require(join(process.cwd(), "./webpack.config.js"));
@@ -17,7 +20,7 @@ if (hasFlag("auto-webpack-config") === true) {
 
 let compilerOutput = "";
 const oldConsoleLog = console.log;
-console.log = (...msg) => compilerOutput += msg.join(" ");
+console.log = (...msg) => (compilerOutput += msg.join(" "));
 
 const compiler = webpack(config);
 
@@ -30,18 +33,17 @@ function emitForWrangler(assets) {
     wasm: null,
     wasm_name: "",
     script: null,
-    compiler_output: compilerOutput,
+    compiler_output: compilerOutput
   };
 
   const wasmModuleAsset = Object.keys(assets).find(filterByExtension("wasm"));
   const jsAssets = Object.keys(assets).filter(filterByExtension("js"));
   const hasWasmModule = wasmModuleAsset !== undefined;
 
-  bundle.script =
-    jsAssets.reduce((acc, k) => {
-      const asset = assets[k];
-      return acc + asset.source();
-    }, "");
+  bundle.script = jsAssets.reduce((acc, k) => {
+    const asset = assets[k];
+    return acc + asset.source();
+  }, "");
 
   if (hasWasmModule === true) {
     bundle.wasm = Buffer.from(assets[wasmModuleAsset].source()).toString();
@@ -56,7 +58,7 @@ compiler.run((err, stats) => {
     throw err;
   }
 
-  console.log =  oldConsoleLog;
+  console.log = oldConsoleLog;
   emitForWrangler(stats.compilation.assets);
   // console.log(stats.toString({ colors: true }));
 });
