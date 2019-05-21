855bfeff30de4970860dc31cb37dcb41a7a7f2b6
Sven Sauleau <sven@cloudflare.com>
2019-05-21 17:37:57 +0100

    feat: change default project_type in generate

--- src/commands/generate.rs
+++ src/commands/generate.rs
@@ -42,5 +42,5 @@ fn project_type(template: &str) -> ProjectType {
     if template.contains("rust") {
         return ProjectType::Rust;
     }
-    ProjectType::JavaScript
+    ProjectType::Webpack
 }
