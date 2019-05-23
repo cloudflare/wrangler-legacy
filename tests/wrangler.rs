e86ba80fb214d90e11756a7a55808dacbe8b01e1
Ashley Williams <ashley666ashley@gmail.com>
2019-05-23 12:59:45 -0500

    test(generate): with defaults and with args

--- tests/wrangler.rs
+++ tests/wrangler.rs
@@ -5,12 +5,40 @@ use std::path::Path;
 use std::process::Command;
 
 #[test]
-fn it_generates() {
-    let mut wrangler = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-    wrangler.arg("generate").assert();
+fn it_generates_with_defaults() {
+    let name = "worker";
+    generate(None, None);
 
-    assert_eq!(Path::new("worker").exists(), true);
-    assert_eq!(Path::new("worker/wrangler.toml").exists(), true);
-    fs::remove_dir_all("worker").unwrap();
-    assert_eq!(Path::new("worker").exists(), false);
+    assert_eq!(Path::new(name).exists(), true);
+
+    let wranglertoml_path = format!("{}/wrangler.toml", name);
+    assert_eq!(Path::new(&wranglertoml_path).exists(), true);
+    cleanup(name);
+}
+
+#[test]
+fn it_generates_with_arguments() {
+    let name = "example";
+    let template = "https://github.com/cloudflare/rustwasm-worker-template";
+    generate(Some(name), Some(template));
+
+    assert_eq!(Path::new(name).exists(), true);
+
+    let wranglertoml_path = format!("{}/wrangler.toml", name);
+    assert_eq!(Path::new(&wranglertoml_path).exists(), true);
+    cleanup(name);
+}
+
+fn generate(name: Option<&str>, template: Option<&str>) {
+    let mut wrangler = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    if name.is_none() && template.is_none() {
+        wrangler.arg("generate").assert();
+    } else if name.is_some() && template.is_some() {
+        wrangler.arg("generate").arg(name.unwrap()).arg(template.unwrap()).assert();
+    }
+}
+
+fn cleanup(name: &str) {
+    fs::remove_dir_all(name).unwrap();
+    assert_eq!(Path::new(name).exists(), false);
 }
