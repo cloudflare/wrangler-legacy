9351beac9644d01d7184076dcf42070a4b9f51d1
Aaron O'Mullan <aaron.omullan@gmail.com>
2019-06-17 23:15:46 +0200

    publish: improve remote/local diff
    Allowing us to correctly detect modified & deleted routes

--- src/commands/publish/route.rs
+++ src/commands/publish/route.rs
@@ -1,4 +1,5 @@
 use crate::http;
+use std::collections::HashMap;
 use crate::settings::global_user::GlobalUser;
 use crate::settings::project::Project;
 use reqwest::header::CONTENT_TYPE;
@@ -8,6 +9,7 @@ use log::info;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Route {
+    id: Option<String>, // ID if remote
     script: Option<String>,
     pub pattern: String,
 }
@@ -19,6 +21,7 @@ struct RoutesResponse {
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct ProjectRoutes {
+    script: String,
     routes: Vec<Route>,
 }
 
@@ -26,13 +29,13 @@ pub struct ProjectRoutes {
 pub struct RoutesDiff {
     added: Vec<Route>,
     modified: Vec<Route>,
-    // remotes: Vec<Route>, // TODO: turn into a HashMap for easy lookup by pattern
-    // removed: Option<Vec<Route>>,
+    deleted: Vec<Route>,
 }
 
 impl ProjectRoutes {
     pub fn new(project: &Project) -> Result<ProjectRoutes, failure::Error> {
         Ok(ProjectRoutes {
+            script: project.name.clone(),
             routes: get_local_routes(project)
                 .expect("Could not get local routes from wranger.toml"),
         })
@@ -49,14 +52,26 @@ impl ProjectRoutes {
 
     // publish syncs our locally defined routes with Cloudflare
     // creating, updating or deleting routes as necessary
-    // TODO: needs to be rethought along with diff
     pub fn publish(&self, user: &GlobalUser, project: &Project) -> Result<(), failure::Error> {
         let diff = self.diff(user, project)?;
-        // Add new routes
-        for route in diff.added {
-            create(user, project, &route)?;
+
+        // Then: delete, modify, add
+
+        // Delete old routes (that pointed to this script)
+        for route in diff.deleted {
+            // TODO: delete
+            println!(
+                "⚠️ Wrangler can't yet delete routes: '{pattern}' [#{id}] -> '{script}'",
+                pattern = route.pattern,
+                script = route.script.unwrap_or_default(),
+                id = route.id.unwrap_or_default(),
+            )
         }
+
         // Modify existing routes (patterns pointing to a different script)
+        // TODO:
+        // 1. Safely modify when pattern pointed to this script or "nothing" (i.e: on/off)
+        // 2. Require a --force flag when pattern pointed to another script
         for route in diff.modified {
             println!(
                 "⚠️ Wrangler can't yet sync modified routes: '{pattern}' -> '{script}'",
@@ -64,34 +79,41 @@ impl ProjectRoutes {
                 script = route.script.unwrap_or_default()
             )
         }
+
+        // Add new routes
+        for route in diff.added {
+            create(user, project, &route)?;
+        }
+
         Ok(())
     }
 
-    // diffs local and remote diffs
-    // TODO: the original code was designed to only add paths which might get messy
-    // the whole route-sync logic needs to be rethought IMHO
+    // diffs local and remote routes
     pub fn diff(&self, user: &GlobalUser, project: &Project) -> Result<RoutesDiff, failure::Error> {
-        let remote_routes = get_remote_routes(user, project)?;
+        let remotes = get_remote_routes(user, project)?;
+
+        // Hashmaps for easier lookups
+        let remotes_map = routes_by_pattern(&remotes);
+        let locals_map = routes_by_pattern(&self.routes);
+
+        // Shorthand to tell if a route points to this project
+        let same = |r: &Route| match &r.script {
+            Some(name) => *name == self.script,
+            None => false,
+        };
+
         // Routes to be created (defined locally, but no-matching remote pattern)
-        let new_routes = self
-            .routes
-            .iter()
-            .filter(|r| !remote_routes.iter().any(|rr| r.pattern == rr.pattern))
-            .cloned()
-            .collect::<Vec<Route>>();
+        let added = _where(&self.routes, |r| !remotes_map.contains_key(&r.pattern));
         // Routes to be modified (pattern existed, but is routed to a different script)
-        let modified_routes = self
-            .routes
-            .iter()
-            .filter(|r| !remote_routes.iter().any(|rr| r.pattern == rr.pattern))
-            .cloned()
-            .collect::<Vec<Route>>();
+        let modified = _where(&self.routes, |&r| !same(r) && remotes_map.contains_key(&r.pattern));
+        // Remote routes to be deleted (that aren't specified in this script's config)
+        let deleted = _where(&remotes, |&r| same(r) && !locals_map.contains_key(&r.pattern));
+
         // Diff
-        // TODO: explore delete (see above comments on publish/diff)
         Ok(RoutesDiff {
-            added: new_routes,
-            modified: modified_routes,
-            // remotes: remote_routes,
+            added: added,
+            modified: modified,
+            deleted: deleted,
         })
     }
 }
@@ -102,28 +124,60 @@ impl Route {
     }
 }
 
+fn _where(routes: &Vec<Route>, f: impl Fn(&&Route)->bool) -> Vec<Route> {
+    return routes
+        .iter()
+        .filter(f)
+        .cloned()
+        .collect::<Vec<Route>>();
+}
+
+fn routes_by_pattern(routes: &Vec<Route>) -> HashMap<String, Route> {
+    return routes.iter().fold(HashMap::new(), |mut acc, route| {
+        acc.insert(route.pattern.clone(), route.clone());
+        return acc;
+    });
+}
+
 fn get_local_routes(project: &Project) -> Result<Vec<Route>, failure::Error> {
     let mut routes: Vec<Route> = vec![];
-    if project.route.is_some() {
-        // Single route
-        routes = vec![Route {
-            script: Some(project.name.to_string()),
-            pattern: project.route.clone().unwrap().to_string(),
-        }];
-    } else if project.routes.is_some() && (project.routes.clone().unwrap().len() > 0) {
-        // Build routes from project's HashMap
 
-        for (pattern, script) in project.routes.clone().unwrap().iter() {
-            routes.push(Route {
-                script: Some(script.to_string()),
-                pattern: pattern.to_string(),
-            })
-        }
-    } else {
+    // No specified routes
+    if !(project.route.is_some() || project.routes_on.is_some()) {
         failure::bail!(
             "You must provide a route or routes in your wrangler.toml before publishing!"
         );
     }
+
+    // All routes will point to the project's script (AKA it's name)
+    let script_name = project.name.to_string();
+
+    // Single route
+    if project.route.is_some() {
+        return Ok(vec![Route {
+            id: None,
+            script: Some(script_name),
+            pattern: project.route.clone().unwrap().to_string(),
+        }]);;
+    }
+
+    // Enabled routes
+    for pattern in project.routes_on.clone().unwrap_or_default().iter() {
+        routes.push(Route {
+            id: None,
+            script: Some(script_name.clone()),
+            pattern: pattern.to_string(),
+        })
+    }
+    // Disabled routes
+    for pattern in project.routes_off.clone().unwrap_or_default().iter() {
+        routes.push(Route {
+            id: None,
+            script: None,
+            pattern: pattern.to_string(),
+        })
+    }
+
     return Ok(routes);
 }
 
