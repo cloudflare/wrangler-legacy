f1b72467abe0ae5780a3e37c735bac2e7f5e80d2
Matthew Alonso <granjef3@gmail.com>
2019-06-28 09:32:52 -0500

    moved fs read to success branch

--- src/commands/build/wranglerjs/mod.rs
+++ src/commands/build/wranglerjs/mod.rs
@@ -33,10 +33,11 @@ pub fn run_build(cache: &Cache, project: &Project) -> Result<(), failure::Error>
     info!("Running {:?}", command);
 
     let status = command.status()?;
-    let output = fs::read_to_string(temp_file.clone()).expect("could not retrieve ouput");
-    fs::remove_file(temp_file)?;
 
     if status.success() {
+        let output = fs::read_to_string(temp_file.clone()).expect("could not retrieve ouput");
+        fs::remove_file(temp_file)?;
+
         let wranglerjs_output: WranglerjsOutput =
             serde_json::from_str(&output).expect("could not parse wranglerjs output");
 
@@ -59,6 +60,7 @@ pub fn run_build(cache: &Cache, project: &Project) -> Result<(), failure::Error>
         message::success(&msg);
         Ok(())
     } else {
+        fs::remove_file(temp_file)?;
         failure::bail!("failed to execute `{:?}`: exited with {}", command, status)
     }
 }
