a0922a9d6a3533bed678241942e17759b5936eee
Matthew Alonso <granjef3@gmail.com>
2019-07-15 16:46:05 -0500

    Check origin header and close if invalid

--- src/commands/publish/preview/mod.rs
+++ src/commands/publish/preview/mod.rs
@@ -22,7 +22,7 @@ use crate::terminal::message;
 
 use std::sync::mpsc::channel;
 use std::thread;
-use ws::WebSocket;
+use ws::{CloseCode, Handler, Handshake, Result as WSResult, Sender, WebSocket};
 
 pub fn preview(
     project: &Project,
@@ -158,10 +158,51 @@ enum FiddleMessageData {
     LiveReload { new_id: String },
 }
 
+struct FiddleMessageServer {
+    out: Sender,
+}
+
+impl Handler for FiddleMessageServer {
+    fn on_open(&mut self, handshake: Handshake) -> WSResult<()> {
+        #[cfg(not(debug_assertions))]
+        const SAFE_ORIGINS: &[&str] = &[
+            "https://cloudflareworkers.com/",
+        ];
+
+        #[cfg(debug_assertions)]
+        const SAFE_ORIGINS: &[&str] = &[
+            "https://cloudflareworkers.com/",
+            "https://localhost", //trailing slash ommitted to allow for any port
+        ];
+
+        let origin = handshake.request.origin()?.unwrap_or("unknown");
+
+        let is_safe = SAFE_ORIGINS.iter().fold(false, |is_safe, safe_origin| {
+            is_safe || origin.starts_with(safe_origin)
+        });
+
+        if is_safe {
+            message::info(&format!("Accepted connection from {}", origin));
+        } else {
+            message::user_error(&format!(
+                "Denied connection from {}. This is not a trusted origin",
+                origin
+            ));
+
+            let _ = self
+                .out
+                .close(CloseCode::Policy)
+                .expect("failed to close connection to unsafe origin");
+        }
+
+        Ok(())
+    }
+}
+
 fn watch_for_changes(session_id: String, ws_port: u16) -> Result<(), failure::Error> {
     //start up the websocket server.
-    //needs a bs handler factory closure, even though we never respond
-    let server = WebSocket::new(|_out| |_msg| Ok(()))?.bind(format!("localhost:{}", ws_port))?;
+    let server = WebSocket::new(|out| FiddleMessageServer { out })?
+        .bind(format!("localhost:{}", ws_port))?;
     let broadcaster = server.broadcaster();
     thread::spawn(move || server.run());
 
