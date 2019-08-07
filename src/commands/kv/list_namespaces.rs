3900fb6f75909ffd939a60b27949c7a9c332a8be
Ashley Lewis <ashleymichal@gmail.com>
2019-08-07 12:08:39 -0500

    Pretty up list output

--- src/commands/kv/list_namespaces.rs
+++ src/commands/kv/list_namespaces.rs
@@ -1,6 +1,8 @@
 use cloudflare::apiclient::APIClient;
-
 use cloudflare::workerskv::list_namespaces::ListNamespaces;
+use cloudflare::workerskv::WorkersKVNamespace;
+
+use prettytable::{Cell, Row, Table};
 
 use crate::terminal::message;
 
@@ -8,16 +10,32 @@ pub fn list_namespaces() -> Result<(), failure::Error> {
     let client = super::api_client()?;
     let account_id = super::account_id()?;
 
-    message::working("Listing namespaces");
+    message::working("Fetching namespaces...");
 
     let response = client.request(&ListNamespaces {
         account_identifier: &account_id,
     });
 
     match response {
-        Ok(success) => message::success(&format!("Success: {:#?}", success.result)),
+        Ok(success) => {
+            let table = namespace_table(success.result);
+            message::success(&format!("Success: \n{}", table));
+        }
         Err(e) => super::print_error(e),
     }
 
     Ok(())
+}
+
+fn namespace_table(namespaces: Vec<WorkersKVNamespace>) -> Table {
+    let mut table = Table::new();
+    let table_head = Row::new(vec![Cell::new("TITLE"), Cell::new("ID")]);
+
+    table.add_row(table_head);
+    for ns in namespaces {
+        let row = Row::new(vec![Cell::new(&ns.title), Cell::new(&ns.id)]);
+        table.add_row(row);
+    }
+
+    table
 }
