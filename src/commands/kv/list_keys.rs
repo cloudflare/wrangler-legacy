7cc55f31510de258aea4d4bb00502d5c557837d7
Gabbi Fisher <gabbi@cloudflare.com>
2019-08-21 16:30:21 -0700

    cleanup

--- src/commands/kv/list_keys.rs
+++ src/commands/kv/list_keys.rs
@@ -32,9 +32,6 @@ pub fn list_keys(id: &str, prefix: Option<&str>) -> Result<(), failure::Error> {
 
     // Iterate over all pages until no pages of keys are left.
     // This is detected when a returned cursor is an empty string.
-    // todo(gabbi): the code in this loop is the product of a looooong fight
-    // with the borrow checker. Please tell me if there's a neater way to write
-    // the logic below!
     loop {
         let (result, cursor) = match response {
             Ok(success) => (
@@ -53,15 +50,10 @@ pub fn list_keys(id: &str, prefix: Option<&str>) -> Result<(), failure::Error> {
             }
             Some(_) => {
                 // Case where we still have pages to iterate through (a cursor is returned).
+                print_page(result, false)?;
+
                 // Update cursor in request_params.params, and make another request to Workers KV API.
                 request_params.params.cursor = cursor;
-
-                // todo(gabbi): Right now, I print out the results of every page
-                // as wrangler gets them (instead of storing them in memory and
-                // outputting them all at once). What do the reviewers think about this?
-                // I figured this was the best option because it wouldn't eat memory, but
-                // I'm curious what other folks think.
-                print_page(result, false)?;
                 response = client.request(&request_params);
             }
         }
