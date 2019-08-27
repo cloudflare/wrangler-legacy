41bb7e2da8f83375b24690b4b6eccc5ba6aabb35
Gabbi Fisher <gabbi@cloudflare.com>
2019-08-27 08:37:36 -0700

    fix tests

--- tests/config.rs
+++ tests/config.rs
@@ -23,6 +23,7 @@ fn generate_config_with(eol: &str) {
 
     write!(stdin, "email@example.com{}", eol).unwrap();
     write!(stdin, "apikeythisissecretandlong{}", eol).unwrap();
+    write!(stdin, "{}", eol).unwrap();
 
     let mut buffer = "".to_string();
     let mut stdout = cmd.stdout.expect("stdout");
