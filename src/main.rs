ad0a5cb8ff209ceadf3e7490dbe9341e60e3a77b
Gabbi Fisher <gabbi@cloudflare.com>
2019-09-10 10:38:23 -0700

    Update src/main.rs
    Co-Authored-By: Ashley Michal <ashleymichal@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -279,7 +279,7 @@ fn run() -> Result<(), failure::Error> {
                         .about("Delete multiple keys and their values from a namespace")
                         .arg(
                             Arg::with_name("namespace")
-                            .help("The title of the namespace this action applies to")
+                            .help("The binding of the namespace this action applies to")
                             .required(true)
                             .index(1)
                         )
