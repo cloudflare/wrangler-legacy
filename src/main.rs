0923b0dde86e30b1e907b04eb741181499c52dd9
Avery Harnish <averyharnish@gmail.com>
2019-09-17 10:59:49 -0500

    Different emojis for different commands

--- src/main.rs
+++ src/main.rs
@@ -77,7 +77,7 @@ fn run() -> Result<(), failure::Error> {
             SubCommand::with_name("kv:namespace")
                 .about(&*format!(
                     "{} Interact with your Workers KV Namespaces",
-                    emoji::KV
+                    emoji::FILES
                 ))
                 .setting(AppSettings::SubcommandRequiredElseHelp)
                 .subcommand(
@@ -107,7 +107,7 @@ fn run() -> Result<(), failure::Error> {
             .subcommand(SubCommand::with_name("kv:key")
                 .about(&*format!(
                     "{} Individually manage Workers KV key-value pairs",
-                    emoji::KV
+                    emoji::KEY
                 ))
                 .setting(AppSettings::SubcommandRequiredElseHelp)
                 .subcommand(
@@ -202,7 +202,7 @@ fn run() -> Result<(), failure::Error> {
             SubCommand::with_name("kv:bulk")
                 .about(&*format!(
                     "{} Interact with multiple Workers KV key-value pairs at once",
-                    emoji::KV
+                    emoji::BICEP
                 ))
                 .setting(AppSettings::SubcommandRequiredElseHelp)
                 .subcommand(
