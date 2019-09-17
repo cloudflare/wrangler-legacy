054ee162bf2e38bf18683c9ce85e0b34fe17f001
Ashley Lewis <ashleymichal@gmail.com>
2019-09-17 09:44:19 -0500

    Add logic for getting/creating Site namespace

---
+++ src/commands/kv/namespace/site.rs
@@ -0,0 +1,60 @@
+use cloudflare::endpoints::workerskv::create_namespace::CreateNamespace;
+use cloudflare::endpoints::workerskv::create_namespace::CreateNamespaceParams;
+use cloudflare::endpoints::workerskv::list_namespaces::ListNamespaces;
+use cloudflare::endpoints::workerskv::WorkersKvNamespace;
+use cloudflare::framework::apiclient::ApiClient;
+use cloudflare::framework::response::ApiFailure;
+
+use crate::commands::kv;
+use crate::settings::global_user::GlobalUser;
+use crate::settings::target::Target;
+use crate::terminal::message;
+
+pub fn site(target: &Target, user: &GlobalUser) -> Result<WorkersKvNamespace, failure::Error> {
+    let client = kv::api_client(user.to_owned())?;
+
+    let title = format!("__{}-{}", target.name, "workers_sites_assets");
+    let msg = format!("Creating namespace for Workers Site \"{}\"", title);
+    message::working(&msg);
+
+    let response = client.request(&CreateNamespace {
+        account_identifier: &target.account_id,
+        params: CreateNamespaceParams {
+            title: title.to_owned(),
+        },
+    });
+
+    match response {
+        Ok(success) => {
+            return Ok(success.result);
+        }
+        Err(e) => match e {
+            ApiFailure::Error(_status, api_errors) => {
+                if api_errors
+                    .errors
+                    .iter()
+                    .find(|&e| e.code == 10014)
+                    .is_some()
+                {
+                    log::info!("Namespace {} already exists.", title);
+                    let response = client.request(&ListNamespaces {
+                        account_identifier: &target.account_id,
+                    });
+
+                    match response {
+                        Ok(success) => Ok(success
+                            .result
+                            .iter()
+                            .find(|ns| ns.title == title)
+                            .unwrap()
+                            .to_owned()),
+                        Err(e) => failure::bail!("{:?}", e),
+                    }
+                } else {
+                    failure::bail!("{:?}", api_errors.errors)
+                }
+            }
+            ApiFailure::Invalid(reqwest_err) => failure::bail!("Error: {}", reqwest_err),
+        },
+    }
+}
