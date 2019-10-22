ae7a62dce25b6b43a29ce3e7b833fa8e7250e867
Gabbi Fisher <gabbi@cloudflare.com>
2019-10-21 17:04:53 -0700

    Fix path parsing logic; this should work for OSX now...

--- src/commands/build/watch/watcher.rs
+++ src/commands/build/watch/watcher.rs
@@ -1,5 +1,6 @@
 use notify::DebouncedEvent;
 use std::collections::HashSet;
+use std::env;
 use std::path::PathBuf;
 use std::sync::mpsc::Receiver;
 use std::time::Duration;
@@ -40,64 +41,72 @@ fn get_changed_path_from_event(
     ignore_dirs: Option<&HashSet<String>>,
 ) -> Result<Option<PathBuf>, Error> {
     info!("debounced event is {:?}", event);
+    let pwd = env::current_dir()?;
     match event {
         DebouncedEvent::Error(error, _) => Err(format_err!("{:?}", error)),
-        DebouncedEvent::NoticeWrite(path) => Ok(filter_ignored(path, ignore_dirs)),
-        DebouncedEvent::Write(path) => Ok(filter_ignored(path, ignore_dirs)),
-        DebouncedEvent::NoticeRemove(path) => Ok(filter_ignored(path, ignore_dirs)),
-        DebouncedEvent::Remove(path) => Ok(filter_ignored(path, ignore_dirs)),
-        DebouncedEvent::Create(path) => Ok(filter_ignored(path, ignore_dirs)),
+        DebouncedEvent::NoticeWrite(path) => Ok(filter_ignored(pwd, path, ignore_dirs)?),
+        DebouncedEvent::Write(path) => Ok(filter_ignored(pwd, path, ignore_dirs)?),
+        DebouncedEvent::NoticeRemove(path) => Ok(filter_ignored(pwd, path, ignore_dirs)?),
+        DebouncedEvent::Remove(path) => Ok(filter_ignored(pwd, path, ignore_dirs)?),
+        DebouncedEvent::Create(path) => Ok(filter_ignored(pwd, path, ignore_dirs)?),
         _ => Ok(None),
     }
 }
 
 // Exclude files within ignored directories from live reload.
-fn filter_ignored(path: PathBuf, ignore_dirs_opt: Option<&HashSet<String>>) -> Option<PathBuf> {
-    // let path_str = path.to_str();
-    // if path_str.is_none() {
-    //     return None;
-    // }
-
-    // let mut absolute_path: Vec<&str> = path_str.unwrap().split("/./").collect();
-    // let relative_path = absolute_path.pop();
-    // if relative_path.is_none() {
-    //     return None;
-    // }
-
-    
+fn filter_ignored(
+    pwd: PathBuf,
+    path: PathBuf,
+    ignore_dirs_opt: Option<&HashSet<String>>,
+) -> Result<Option<PathBuf>, failure::Error> {
+    let path = path.canonicalize()?;
+    let relative_path = path.strip_prefix(pwd)?;
 
     // Check if file in ignored directory.
     if let Some(ignore_dirs) = ignore_dirs_opt {
         for ignore_dir in ignore_dirs {
-            if let Some(relative) = relative_path {
-                if relative.starts_with(ignore_dir) {
-                    return None;
-                }
+            if relative_path.starts_with(ignore_dir) {
+                return Ok(None);
             }
         }
     }
-    Some(path)
+    Ok(Some(path))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::collections::HashSet;
-    use std::path::Path;
+    use std::env;
+    use std::fs;
 
     #[test]
     fn it_can_filter_ignored_path() {
         let to_ignore = ["pkg", "target", "worker/generated"];
         let ignore_dirs: HashSet<_> = to_ignore.iter().map(|d| d.to_string()).collect();
-        let test_path = "home/blah/worker/generated/file.txt";
-        assert!(filter_ignored(Path::new(test_path).to_path_buf(), Some(&ignore_dirs)).is_none());
+        let pwd = env::current_dir().unwrap();
+        let test_dir = pwd.join("my-test-worker1");
+        let test_path = test_dir.join("worker/generated/file.txt");
+        fs::create_dir_all(test_path.clone()).unwrap();
+
+        let outcome = filter_ignored(test_dir.to_path_buf(), test_path, Some(&ignore_dirs));
+        fs::remove_dir_all(test_dir).unwrap();
+        assert!(outcome.is_ok());
+        assert!(outcome.unwrap().is_none());
     }
 
     #[test]
     fn it_can_include_src_file() {
         let to_ignore = ["pkg", "target", "worker/generated"];
         let ignore_dirs: HashSet<_> = to_ignore.iter().map(|d| d.to_string()).collect();
-        let test_path = "home/blah/src/lib.rs";
-        assert!(filter_ignored(Path::new(test_path).to_path_buf(), Some(&ignore_dirs)).is_some());
+        let pwd = env::current_dir().unwrap();
+        let test_dir = pwd.join("my-test-worker2");
+        let test_path = test_dir.join("src/lib.rs");
+        fs::create_dir_all(test_path.clone()).unwrap();
+
+        let outcome = filter_ignored(test_dir.to_path_buf(), test_path, Some(&ignore_dirs));
+        // fs::remove_dir_all(test_dir).unwrap();
+        assert!(outcome.is_ok());
+        assert!(outcome.unwrap().is_some());
     }
 }
