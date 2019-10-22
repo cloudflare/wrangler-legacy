3caf88225dd4d912696a9c13fb1e2a479ef58ea1
Avery Harnish <averyharnish@gmail.com>
2019-10-22 10:20:00 -0500

    Update generate --site error message

--- src/main.rs
+++ src/main.rs
@@ -390,12 +390,12 @@ fn run() -> Result<(), failure::Error> {
     } else if let Some(matches) = matches.subcommand_matches("generate") {
         let name = matches.value_of("name").unwrap_or("worker");
         let site = matches.is_present("site");
-        let template_value = matches.value_of("template");
+        let template = matches.value_of("template");
         let mut target_type = None;
 
         let template = if site {
-            if template_value.is_some() {
-                failure::bail!("You cannot specify a template when generating a Workers Site. If you want to generate site boilerplate, run wrangler generate --site")
+            if template.is_some() {
+                failure::bail!("You cannot specify a template when generating a Workers Site. If you'd like to use the default site boilerplate, run wrangler generate --site. If you'd like to use another site boilerplate, omit the --site when running wrangler generate.")
             }
             "https://github.com/cloudflare/worker-sites-template"
         } else {
@@ -404,7 +404,7 @@ fn run() -> Result<(), failure::Error> {
             }
 
             let default_template = "https://github.com/cloudflare/worker-template";
-            let template = template_value.unwrap_or(match target_type {
+            let template = template.unwrap_or(match target_type {
                 Some(ref pt) => match pt {
                     TargetType::Rust => "https://github.com/cloudflare/rustwasm-worker-template",
                     _ => default_template,
