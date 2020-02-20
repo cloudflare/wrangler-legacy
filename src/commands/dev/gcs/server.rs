0ee938ddd70def50b0e36eb430950c06fdd69864
Avery Harnish <averyharnish@gmail.com>
2020-02-20 15:41:26 -0600

    cargo fmt

--- src/commands/dev/gcs/server.rs
+++ src/commands/dev/gcs/server.rs
@@ -18,59 +18,59 @@ pub(super) async fn serve(
     server_config: ServerConfig,
     preview_id: Arc<Mutex<String>>,
 ) -> Result<(), failure::Error> {
-// set up https client to connect to the preview service
-let https = HttpsConnector::new();
-let client = HyperClient::builder().build::<_, Body>(https);
+    // set up https client to connect to the preview service
+    let https = HttpsConnector::new();
+    let client = HyperClient::builder().build::<_, Body>(https);
 
-let listening_address = server_config.listening_address.clone();
-// create a closure that hyper will use later to handle HTTP requests
-let make_service = make_service_fn(move |_| {
-    let client = client.to_owned();
-    let preview_id = preview_id.lock().unwrap().to_owned();
-    let server_config = server_config.to_owned();
-    async move {
-        Ok::<_, failure::Error>(service_fn(move |req| {
-            let client = client.to_owned();
-            let preview_id = preview_id.to_owned();
-            let server_config = server_config.to_owned();
-            let version = req.version();
-            let (parts, body) = req.into_parts();
-            let req_method = parts.method.to_string();
-            let now: DateTime<Local> = Local::now();
-            let path = get_path_as_str(&parts.uri);
-            async move {
-                let resp = preview_request(
-                    Request::from_parts(parts, body),
-                    client,
-                    preview_id.to_owned(),
-                )
-                .await?;
-                let (mut parts, body) = resp.into_parts();
+    let listening_address = server_config.listening_address.clone();
+    // create a closure that hyper will use later to handle HTTP requests
+    let make_service = make_service_fn(move |_| {
+        let client = client.to_owned();
+        let preview_id = preview_id.lock().unwrap().to_owned();
+        let server_config = server_config.to_owned();
+        async move {
+            Ok::<_, failure::Error>(service_fn(move |req| {
+                let client = client.to_owned();
+                let preview_id = preview_id.to_owned();
+                let server_config = server_config.to_owned();
+                let version = req.version();
+                let (parts, body) = req.into_parts();
+                let req_method = parts.method.to_string();
+                let now: DateTime<Local> = Local::now();
+                let path = get_path_as_str(&parts.uri);
+                async move {
+                    let resp = preview_request(
+                        Request::from_parts(parts, body),
+                        client,
+                        preview_id.to_owned(),
+                    )
+                    .await?;
+                    let (mut parts, body) = resp.into_parts();
 
-                destructure_response(&mut parts)?;
-                let resp = Response::from_parts(parts, body);
+                    destructure_response(&mut parts)?;
+                    let resp = Response::from_parts(parts, body);
 
-                println!(
-                    "[{}] {} {}{} {:?} {}",
-                    now.format("%Y-%m-%d %H:%M:%S"),
-                    req_method,
-                    server_config.host,
-                    path,
-                    version,
-                    resp.status()
-                );
-                Ok::<_, failure::Error>(resp)
-            }
-        }))
+                    println!(
+                        "[{}] {} {}{} {:?} {}",
+                        now.format("%Y-%m-%d %H:%M:%S"),
+                        req_method,
+                        server_config.host,
+                        path,
+                        version,
+                        resp.status()
+                    );
+                    Ok::<_, failure::Error>(resp)
+                }
+            }))
+        }
+    });
+
+    let server = Server::bind(&listening_address.address).serve(make_service);
+    println!("{} Listening on http://{}", emoji::EAR, listening_address);
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
     }
-});
-
-let server = Server::bind(&listening_address.address).serve(make_service);
-println!("{} Listening on http://{}", emoji::EAR, listening_address);
-if let Err(e) = server.await {
-    eprintln!("server error: {}", e);
-}
-Ok(())
+    Ok(())
 }
 
 fn get_preview_url(path_string: &str) -> Result<Uri, InvalidUri> {
@@ -88,7 +88,6 @@ fn preview_request(
     req: Request<Body>,
     client: HyperClient<HttpsConnector<HttpConnector>>,
     preview_id: String,
-    server_config: ServerConfig,
 ) -> ResponseFuture {
     let (mut parts, body) = req.into_parts();
 
