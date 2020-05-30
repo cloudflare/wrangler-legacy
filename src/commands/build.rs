55c567b8bbed1f0531dbced729289f6e7e2a9344
Luan Raithz Machado <luanraithz@gmail.com>
2020-05-30 19:50:02 -0300

    Changed to unwrap_or

--- src/commands/build.rs
+++ src/commands/build.rs
@@ -9,11 +9,7 @@ use super::DEFAULT_CONFIG_PATH;
 
 pub fn run(matches: &ArgMatches) -> Result<(), failure::Error> {
     log::info!("Getting project settings");
-    let config_file = if let Some(file) = matches.value_of("file") {
-        file
-    } else {
-        DEFAULT_CONFIG_PATH
-    };
+    let config_file = matches.value_of("file").unwrap_or(DEFAULT_CONFIG_PATH);
     let config_path = Path::new(config_file);
     let manifest = Manifest::new(&config_path)?;
     let env = matches.value_of("env");
