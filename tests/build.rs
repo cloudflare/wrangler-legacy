099e4ad0ed6edd9113c20aad828a404794e0dbab
Luan Raithz Machado <luanraithz@gmail.com>
2020-06-03 21:26:12 -0300

    fmt

--- tests/build.rs
+++ tests/build.rs
@@ -363,12 +363,15 @@ fn it_builds_with_webpack_name_output_warn() {
     );
 }
 
-fn build_creates_assets_with_arg(fixture: &Fixture, script_names: Vec<&str>, args: Vec<&str>) -> (String, String) {
+fn build_creates_assets_with_arg(
+    fixture: &Fixture,
+    script_names: Vec<&str>,
+    args: Vec<&str>,
+) -> (String, String) {
     let mut build = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     build.current_dir(fixture.get_path());
     build.arg("build");
     build.args(args);
-
 
     let output = build.output().expect("failed to execute process");
     assert!(output.status.success());
