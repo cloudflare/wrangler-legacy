a3656a974541ada6263f1901016342cbfc9fb2aa
jspspike <jspspike@gmail.com>
2020-08-11 20:26:26 -0500

    Made stdout go to /dev/null

--- src/terminal/browser.rs
+++ src/terminal/browser.rs
@@ -1,16 +1,27 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 pub fn open_browser(url: &str) -> Result<(), failure::Error> {
     if cfg!(target_os = "windows") {
         let url_escaped = url.replace("&", "^&");
         let windows_cmd = format!("start {}", url_escaped);
-        Command::new("cmd").args(&["/C", &windows_cmd]).spawn()?;
+        Command::new("cmd")
+            .args(&["/C", &windows_cmd])
+            .stdout(Stdio::null())
+            .spawn()?;
     } else if cfg!(target_os = "linux") {
         let linux_cmd = format!(r#"xdg-open "{}""#, url);
-        Command::new("sh").arg("-c").arg(&linux_cmd).spawn()?;
+        Command::new("sh")
+            .arg("-c")
+            .arg(&linux_cmd)
+            .stdout(Stdio::null())
+            .spawn()?;
     } else {
         let mac_cmd = format!(r#"open "{}""#, url);
-        Command::new("sh").arg("-c").arg(&mac_cmd).spawn()?;
+        Command::new("sh")
+            .arg("-c")
+            .arg(&mac_cmd)
+            .stdout(Stdio::null())
+            .spawn()?;
     };
 
     Ok(())
