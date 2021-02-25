thefill/wrangler

Squashed commit of the following:
commit e648284cdf84b5d65047de818201ff24e5d5f1aa
Author: malonso@cloudflare.com <malonso@cloudflare.com>
Date:   Tue Dec 15 10:56:36 2020 -0600

    better build logging

commit a271ffcb29512ac542cc81184c5221b0d7c770c5
Author: malonso@cloudflare.com <malonso@cloudflare.com>
Date:   Mon Dec 14 11:45:41 2020 -0600

    add script-format field, remove Option<T> from builder config

commit 193ba02115aae64245622f742f1decb88d6e6e2a
Author: malonso@cloudflare.com <malonso@cloudflare.com>
Date:   Fri Dec 11 13:53:30 2020 -0600

    coalesce bundler TargetType under javascript, using config to distinguish

commit db9c356542d85b37117e5f01979d183c6474e947
Author: malonso@cloudflare.com <malonso@cloudflare.com>
Date:   Fri Dec 11 15:09:39 2020 -0600

    rename bundle_config to builder_config, and related names

commit e5780a04f105ede6c0627e63616ab55c94fc3836
Author: malonso@cloudflare.com <malonso@cloudflare.com>
Date:   Fri Dec 11 13:22:14 2020 -0600

    remove check linter

commit 1c3b5884d2e3a653e649348b40d1d93d6f8229aa
Merge: 2a3b5b3 cbbed17
Author: malonso@cloudflare.com <malonso@cloudflare.com>
Date:   Fri Dec 11 13:02:33 2020 -0600

    Merge remote-tracking branch 'caass/master' into malonso/modules-do

commit cbbed17647290422322a73bb177f1761848e21a0
Author: Cassandra Fridkin <cass@swag.lgbt>
Date:   Tue Nov 3 00:47:05 2020 -0500

    Rewrite gzip buffer-building strategy to maybe be friendlier to rayon

commit eeb38cfab60c50157667f59fbe0f1eba93c8f179
Author: Cassandra Fridkin <cass@swag.lgbt>
Date:   Tue Nov 3 00:20:53 2020 -0500

    forgot to run clippy again lol

commit 6cba17b6f1b826b319efff954ac176f21babebb7
Merge: b000958 04584e5
Author: Cassandra Fridkin <cass@swag.lgbt>
Date:   Tue Nov 3 00:10:03 2020 -0500

    Merge branch 'cloudflare' into master

commit b000958c545219bee240168db1db11088fcc2190
Author: Cassandra Fridkin <cass@swag.lgbt>
Date:   Tue Nov 3 00:07:17 2020 -0500

    Apply suggestions from code review

    - Che

src/build/mod.rs
@@ -12,10 +12,23 @@ use std::process::Command;
 pub fn build_target(target: &Target) -> Result<String, failure::Error> {
     let target_type = &target.target_type;
     match target_type {
-        TargetType::JavaScript => {
-            let msg = "JavaScript project found. Skipping unnecessary build!".to_string();
-            Ok(msg)
-        }
+        TargetType::JavaScript => match &target.builder_config {
+            None => {
+                let msg = "Basic JavaScript project found. Skipping unnecessary build!".to_string();
+                Ok(msg)
+            }
+            Some(config) => {
+                if let Some(mut command) = config.build_command() {
+                    StdErr::working(
+                        format!("Running {}", config.build_command.as_ref().unwrap()).as_ref(),
+                    );
+                    command.spawn()?.wait()?.success();
+                    Ok(String::from("Build completed successfully!"))
+                } else {
+                    Ok(String::from("No build command specified, skipping build."))
+                }
+            }
+        },
         TargetType::Rust => {
             let _ = which::which("rustc").map_err(|e| {
                 failure::format_err!(
@@ -31,6 +44,7 @@ pub fn build_target(target: &Target) -> Result<String, failure::Error> {
             let command = command(&args, &binary_path);
             let command_name = format!("{:?}", command);
 
+            StdErr::working("Compiling your project to WebAssembly...");
             commands::run(command, &command_name)?;
             let msg = "Build succeeded".to_string();
             Ok(msg)
@@ -49,8 +63,6 @@ pub fn build_target(target: &Target) -> Result<String, failure::Error> {
 }
 
 pub fn command(args: &[&str], binary_path: &PathBuf) -> Command {
-    StdErr::working("Compiling your project to WebAssembly...");
-
     let mut c = if cfg!(target_os = "windows") {
         let mut c = Command::new("cmd");
         c.arg("/C");

src/commands/kv/mod.rs
@@ -125,6 +125,7 @@ mod tests {
             site: None,
             vars: None,
             text_blobs: None,
+            builder_config: None,
         };
         assert!(kv::get_namespace_id(&target_with_dup_kv_bindings, "").is_err());
     }

src/settings/toml/environment.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 use serde::{Deserialize, Serialize};
 use serde_with::rust::string_empty_as_none;
 
+use crate::settings::toml::builder::Builder;
 use crate::settings::toml::kv_namespace::ConfigKvNamespace;
 use crate::settings::toml::route::RouteConfig;
 use crate::settings::toml::site::Site;
@@ -21,6 +22,7 @@ pub struct Environment {
     #[serde(default, with = "string_empty_as_none")]
     pub zone_id: Option<String>,
     pub webpack_config: Option<String>,
+    pub builder_config: Option<Builder>,
     pub private: Option<bool>,
     pub site: Option<Site>,
     #[serde(alias = "kv-namespaces")]

src/settings/toml/target.rs
@@ -1,3 +1,4 @@
+use super::builder::Builder;
 use super::kv_namespace::KvNamespace;
 use super::site::Site;
 use super::target_type::TargetType;
@@ -14,6 +15,7 @@ pub struct Target {
     pub name: String,
     pub target_type: TargetType,
     pub webpack_config: Option<String>,
+    pub builder_config: Option<Builder>,
     pub site: Option<Site>,
     pub vars: Option<HashMap<String, String>>,
     pub text_blobs: Option<HashMap<String, PathBuf>>,

src/sites/mod.rs
@@ -319,6 +319,7 @@ mod tests {
             target_type: TargetType::JavaScript,
             webpack_config: None,
             site: Some(site),
+            builder_config: None,
             vars: None,
             text_blobs: None,
         }

src/watch/mod.rs
@@ -3,10 +3,10 @@ use ignore::overrides::OverrideBuilder;
 use ignore::WalkBuilder;
 pub use watcher::wait_for_changes;
 
-use crate::build::command;
 use crate::settings::toml::{Target, TargetType};
 use crate::terminal::message::{Message, StdOut};
 use crate::wranglerjs;
+use crate::{build::command, build_target};
 use crate::{commands, install};
 
 use notify::{self, RecursiveMode, Watcher};
@@ -28,27 +28,56 @@ pub fn watch_and_build(
     tx: Option<mpsc::Sender<()>>,
 ) -> Result<(), failure::Error> {
     let target_type = &target.target_type;
+    let builder_config = target.builder_config.clone();
     match target_type {
         TargetType::JavaScript => {
-            thread::spawn(move || {
+            let target = target.clone();
+            thread::spawn::<_, Result<(), failure::Error>>(move || {
                 let (watcher_tx, watcher_rx) = mpsc::channel();
-                let mut watcher = notify::watcher(watcher_tx, Duration::from_secs(1)).unwrap();
+                let mut watcher = notify::watcher(watcher_tx, Duration::from_secs(1))?;
 
-                watcher
-                    .watch(JAVASCRIPT_PATH, RecursiveMode::Recursive)
-                    .unwrap();
-                StdOut::info(&format!("watching {:?}", &JAVASCRIPT_PATH));
+                match builder_config {
+                    None => {
+                        watcher.watch(JAVASCRIPT_PATH, RecursiveMode::Recursive)?;
+                        StdOut::info(&format!("watching {:?}", &JAVASCRIPT_PATH));
 
-                loop {
-                    match wait_for_changes(&watcher_rx, COOLDOWN_PERIOD) {
-                        Ok(_path) => {
-                            if let Some(tx) = tx.clone() {
-                                tx.send(()).expect("--watch change message failed to send");
+                        loop {
+                            match wait_for_changes(&watcher_rx, COOLDOWN_PERIOD) {
+                                Ok(_path) => {
+                                    if let Some(tx) = tx.clone() {
+                                        tx.send(()).expect("--watch change message failed to send");
+                                    }
+                                }
+                                Err(e) => {
+                                    log::debug!("{:?}", e);
+                                    StdOut::user_error("Something went wrong while watching.")
+                                }
                             }
                         }
-                        Err(e) => {
-                            log::debug!("{:?}", e);
-                            StdOut::user_error("Something went wrong while watching.")
+                    }
+                    Some(config) => {
+                        watcher
+                            .watch(config.src_dir, notify::RecursiveMode::Recursive)
+                            .unwrap();
+
+                        let mut is_first = true;
+
+                        loop {
+                            match watcher_rx.recv() {
+                                Ok(_) => {
+                                    if is_first {
+                                        is_first = false;
+                                        StdOut::info("Ignoring stale first change");
+                                        continue;
+                                    } else {
+                                        let output = build_target(&target).unwrap();
+                                        StdOut::success(&format!("{}\nUploading...", output));
+                                    }
+                                }
+                                Err(_) => {
+                                    StdOut::user_error("Something went wrong while watching.")
+                                }
+                            }
                         }
                     }
                 }

cloudflare/wrangler-legacy

src/build/mod.rs
@@ -12,10 +12,30 @@ use std::process::Command;
 pub fn build_target(target: &Target) -> Result<String, failure::Error> {
     let target_type = &target.target_type;
     match target_type {
-        TargetType::JavaScript => {
-            let msg = "JavaScript project found. Skipping unnecessary build!".to_string();
-            Ok(msg)
-        }
+        TargetType::JavaScript => match &target.build {
+            None => {
+                let msg = "Basic JavaScript project found. Skipping unnecessary build!".to_string();
+                Ok(msg)
+            }
+            Some(config) => {
+                if let Some((cmd_str, mut cmd)) = config.build_command() {
+                    StdErr::working(format!("Running {}", cmd_str).as_ref());
+                    let build_result = cmd.spawn()?.wait()?;
+                    if build_result.success() {
+                        Ok(String::from("Build completed successfully!"))
+                    } else if let Some(code) = build_result.code() {
+                        Err(failure::err_msg(format!(
+                            "Build failed! Status Code: {}",
+                            code
+                        )))
+                    } else {
+                        Err(failure::err_msg("Build failed."))
+                    }
+                } else {
+                    Ok(String::from("No build command specified, skipping build."))
+                }
+            }
+        },
         TargetType::Rust => {
             let _ = which::which("rustc").map_err(|e| {
                 failure::format_err!(
@@ -31,6 +51,7 @@ pub fn build_target(target: &Target) -> Result<String, failure::Error> {
             let command = command(&args, &binary_path);
             let command_name = format!("{:?}", command);
 
+            StdErr::working("Compiling your project to WebAssembly...");
             commands::run(command, &command_name)?;
             let msg = "Build succeeded".to_string();
             Ok(msg)
@@ -49,8 +70,6 @@ pub fn build_target(target: &Target) -> Result<String, failure::Error> {
 }
 
 pub fn command(args: &[&str], binary_path: &PathBuf) -> Command {
-    StdErr::working("Compiling your project to WebAssembly...");
-
     let mut c = if cfg!(target_os = "windows") {
         let mut c = Command::new("cmd");
         c.arg("/C");

src/commands/kv/mod.rs
@@ -125,6 +125,7 @@ mod tests {
             site: None,
             vars: None,
             text_blobs: None,
+            build: None,
         };
         assert!(kv::get_namespace_id(&target_with_dup_kv_bindings, "").is_err());
     }

src/settings/toml/environment.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 use serde::{Deserialize, Serialize};
 use serde_with::rust::string_empty_as_none;
 
+use crate::settings::toml::builder::Builder;
 use crate::settings::toml::kv_namespace::ConfigKvNamespace;
 use crate::settings::toml::route::RouteConfig;
 use crate::settings::toml::site::Site;
@@ -21,6 +22,7 @@ pub struct Environment {
     #[serde(default, with = "string_empty_as_none")]
     pub zone_id: Option<String>,
     pub webpack_config: Option<String>,
+    pub build: Option<Builder>,
     pub private: Option<bool>,
     pub site: Option<Site>,
     #[serde(alias = "kv-namespaces")]

src/settings/toml/target.rs
@@ -1,3 +1,4 @@
+use super::builder::Builder;
 use super::kv_namespace::KvNamespace;
 use super::site::Site;
 use super::target_type::TargetType;
@@ -14,6 +15,7 @@ pub struct Target {
     pub name: String,
     pub target_type: TargetType,
     pub webpack_config: Option<String>,
+    pub build: Option<Builder>,
     pub site: Option<Site>,
     pub vars: Option<HashMap<String, String>>,
     pub text_blobs: Option<HashMap<String, PathBuf>>,

src/sites/mod.rs
@@ -318,6 +318,7 @@ mod tests {
             target_type: TargetType::JavaScript,
             webpack_config: None,
             site: Some(site),
+            build: None,
             vars: None,
             text_blobs: None,
         }

src/watch/mod.rs
@@ -3,10 +3,10 @@ use ignore::overrides::OverrideBuilder;
 use ignore::WalkBuilder;
 pub use watcher::wait_for_changes;
 
-use crate::build::command;
 use crate::settings::toml::{Target, TargetType};
 use crate::terminal::message::{Message, StdOut};
 use crate::wranglerjs;
+use crate::{build::command, build_target};
 use crate::{commands, install};
 
 use notify::{self, RecursiveMode, Watcher};
@@ -28,27 +28,54 @@ pub fn watch_and_build(
     tx: Option<mpsc::Sender<()>>,
 ) -> Result<(), failure::Error> {
     let target_type = &target.target_type;
+    let build = target.build.clone();
     match target_type {
         TargetType::JavaScript => {
-            thread::spawn(move || {
+            let target = target.clone();
+            thread::spawn::<_, Result<(), failure::Error>>(move || {
                 let (watcher_tx, watcher_rx) = mpsc::channel();
-                let mut watcher = notify::watcher(watcher_tx, Duration::from_secs(1)).unwrap();
+                let mut watcher = notify::watcher(watcher_tx, Duration::from_secs(1))?;
 
-                watcher
-                    .watch(JAVASCRIPT_PATH, RecursiveMode::Recursive)
-                    .unwrap();
-                StdOut::info(&format!("watching {:?}", &JAVASCRIPT_PATH));
+                match build {
+                    None => {
+                        watcher.watch(JAVASCRIPT_PATH, RecursiveMode::Recursive)?;
+                        StdOut::info(&format!("watching {:?}", &JAVASCRIPT_PATH));
 
-                loop {
-                    match wait_for_changes(&watcher_rx, COOLDOWN_PERIOD) {
-                        Ok(_path) => {
-                            if let Some(tx) = tx.clone() {
-                                tx.send(()).expect("--watch change message failed to send");
+                        loop {
+                            match wait_for_changes(&watcher_rx, COOLDOWN_PERIOD) {
+                                Ok(_path) => {
+                                    if let Some(tx) = tx.clone() {
+                                        tx.send(()).expect("--watch change message failed to send");
+                                    }
+                                }
+                                Err(e) => {
+                                    log::debug!("{:?}", e);
+                                    StdOut::user_error("Something went wrong while watching.")
+                                }
                             }
                         }
-                        Err(e) => {
-                            log::debug!("{:?}", e);
-                            StdOut::user_error("Something went wrong while watching.")
+                    }
+                    Some(config) => {
+                        config.verify_watch_dir()?;
+                        watcher.watch(config.watch_dir, notify::RecursiveMode::Recursive)?;
+
+                        loop {
+                            match wait_for_changes(&watcher_rx, COOLDOWN_PERIOD) {
+                                Ok(_path) => match build_target(&target) {
+                                    Ok(output) => {
+                                        StdOut::success(&output);
+                                        if let Some(tx) = tx.clone() {
+                                            tx.send(())
+                                                .expect("--watch change message failed to send");
+                                        }
+                                    }
+                                    Err(e) => StdOut::user_error(&e.to_string()),
+                                },
+                                Err(e) => {
+                                    log::debug!("{:?}", e);
+                                    StdOut::user_error("Something went wrong while watching.")
+                                }
+                            }
                         }
                     }
                 }