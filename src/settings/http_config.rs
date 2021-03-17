d307c644121c3ef9be825a3db48857c2f277237a
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2021-03-16 22:02:10 -0500

    Support string or number deserialization of timeout

--- src/settings/http_config.rs
+++ src/settings/http_config.rs
@@ -1,11 +1,17 @@
-use serde::{Deserialize, Serialize};
-use std::{str::FromStr, time::Duration};
+use serde::de::{self, Visitor};
+use serde::{Deserialize, Deserializer, Serialize};
+use std::convert::TryFrom;
+use std::{fmt, str::FromStr};
+use std::{marker::PhantomData, time::Duration};
 
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 pub struct HttpConfig {
-    connect_timeout: Option<u64>,
-    http_timeout: Option<u64>,
-    bulk_timeout: Option<u64>,
+    #[serde(default, deserialize_with = "string_or_number")]
+    pub connect_timeout: Option<u64>,
+    #[serde(default, deserialize_with = "string_or_number")]
+    pub http_timeout: Option<u64>,
+    #[serde(default, deserialize_with = "string_or_number")]
+    pub bulk_timeout: Option<u64>,
 }
 
 impl HttpConfig {
@@ -49,6 +55,70 @@ impl Default for HttpConfig {
     }
 }
 
+/// When reading from the environment, all values are strings. Thus,
+/// deserialization fails when reading from the environment. To combat this,
+/// this deserialization method is used in order to support deserializing valid
+/// number strings and numbers into the correct value.
+///
+/// https://serde.rs/string-or-struct.html
+fn string_or_number<'de, D>(deserializer: D) -> Result<Option<u64>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    // This is a Visitor that forwards string types to T's `FromStr` impl and
+    // forwards map types to T's `Deserialize` impl. The `PhantomData` is to
+    // keep the compiler from complaining about T being an unused generic type
+    // parameter. We need T in order to know the Value type for the Visitor
+    // impl.
+    struct StringOrNumber(PhantomData<fn() -> Option<u64>>);
+
+    impl<'de> Visitor<'de> for StringOrNumber {
+        type Value = Option<u64>;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("`str`, `u64`, or `none`")
+        }
+
+        fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            value.parse::<u64>().map(|n| Some(n)).map_err(|_| {
+                serde::de::Error::invalid_type(serde::de::Unexpected::Str(value), &self)
+            })
+        }
+
+        fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            match u64::try_from(v) {
+                Ok(v) => Ok(Some(v)),
+                Err(_) => Err(serde::de::Error::invalid_type(
+                    serde::de::Unexpected::Signed(v),
+                    &self,
+                )),
+            }
+        }
+
+        fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            Ok(v.into())
+        }
+
+        fn visit_none<E>(self) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            Ok(None)
+        }
+    }
+
+    deserializer.deserialize_any(StringOrNumber(PhantomData))
+}
+
 #[test]
 fn it_reads_timeout_when_specified_otherwise_reads_default_values() {
     // make sure that the values in our test aren't the default values,
@@ -87,4 +157,81 @@ bulk_timeout = 16
         Duration::from_secs(crate::http::DEFAULT_BULK_TIMEOUT_SECONDS),
         manifest.get_bulk_timeout()
     );
+}
+
+/// Ensures we can deserialize strings into numbers as well. This is to support
+/// passing in these values as environment variables.
+#[test]
+fn it_can_deserialize_strings() {
+    // make sure that the values in our test aren't the default values,
+    // wouln't want this test to break if the defaults happen to be the same
+    // as the test
+    assert_ne!(3, crate::http::DEFAULT_CONNECT_TIMEOUT_SECONDS);
+    assert_ne!(8, crate::http::DEFAULT_HTTP_TIMEOUT_SECONDS);
+    assert_ne!(16, crate::http::DEFAULT_BULK_TIMEOUT_SECONDS);
+
+    // assert that reading a config yields the timeouts specified
+    let http_config = HttpConfig::from_str(
+        r#"
+connect_timeout = "3"
+http_timeout = "8"
+bulk_timeout = "16"
+"#,
+    )
+    .unwrap();
+
+    assert_eq!(Duration::from_secs(3), http_config.get_connect_timeout());
+    assert_eq!(Duration::from_secs(8), http_config.get_http_timeout());
+    assert_eq!(Duration::from_secs(16), http_config.get_bulk_timeout());
+}
+
+#[test]
+fn it_uses_defaults_when_nothing() {
+    // assert that a lack of a config uses the defaults
+    let manifest = HttpConfig::from_str("").unwrap();
+
+    assert_eq!(
+        Duration::from_secs(crate::http::DEFAULT_CONNECT_TIMEOUT_SECONDS),
+        manifest.get_connect_timeout()
+    );
+    assert_eq!(
+        Duration::from_secs(crate::http::DEFAULT_HTTP_TIMEOUT_SECONDS),
+        manifest.get_http_timeout()
+    );
+    assert_eq!(
+        Duration::from_secs(crate::http::DEFAULT_BULK_TIMEOUT_SECONDS),
+        manifest.get_bulk_timeout()
+    );
+}
+
+#[test]
+fn it_ignores_skipped_values() {
+    // assert that a lack of a config uses the defaults
+    let manifest = HttpConfig::from_str(
+        r#"
+http_timeout = 69
+"#,
+    )
+    .unwrap();
+
+    assert_eq!(
+        Duration::from_secs(crate::http::DEFAULT_CONNECT_TIMEOUT_SECONDS),
+        manifest.get_connect_timeout()
+    );
+    assert_eq!(Duration::from_secs(69), manifest.get_http_timeout());
+    assert_eq!(
+        Duration::from_secs(crate::http::DEFAULT_BULK_TIMEOUT_SECONDS),
+        manifest.get_bulk_timeout()
+    );
+}
+
+#[test]
+fn it_fails_for_negative_values() {
+    let manifest = HttpConfig::from_str(
+        r#"
+http_timeout = -1
+"#,
+    );
+
+    assert!(manifest.is_err());
 }
