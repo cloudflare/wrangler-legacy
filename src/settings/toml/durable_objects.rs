d0e938a1af1811ec871178758930c9962d746cd5
malonso@cloudflare.com <malonso@cloudflare.com>
2021-03-30 10:56:41 -0500

    allow bindings as alias for durable objects classes section

--- src/settings/toml/durable_objects.rs
+++ src/settings/toml/durable_objects.rs
@@ -4,11 +4,13 @@ use crate::settings::binding::Binding;
 
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
 pub struct DurableObjects {
+    #[serde(alias = "bindings")]
     pub classes: Option<Vec<DurableObjectsClass>>,
 }
 
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
 pub struct DurableObjectsClass {
+    #[serde(alias = "name")]
     pub binding: String,
     pub class_name: String,
     pub script_name: Option<String>,
