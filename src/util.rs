1087d64f2e97c65f63b83ad426f6f77aad752908
Steve Manuel <smanuel@cloudflare.com>
2021-05-13 17:03:16 -0600

    feat: clarify error messages around durable objects beta

--- src/util.rs
+++ src/util.rs
@@ -8,3 +8,5 @@ impl<'a, 'b> ApplyToApp<'a, 'b> for App<'a, 'b> {
         f(self)
     }
 }
+
+pub const TEMP_NOTICE_ES_MODULES_DO_BETA: &str = "While Durable Objects are in Beta, ES Modules are limited to accounts which have opted-in to the Beta. You may do so by following the instructions here: https://developers.cloudflare.com/workers/learning/using-durable-objects";
