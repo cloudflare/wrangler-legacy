7c09dec8e7b79ebd002334149f13534645563ab6
Steve Manuel <nilslice@gmail.com>
2021-05-14 10:19:20 -0600

    Update src/util.rs
    Co-authored-by: Ashcon Partovi <ashcon@cloudflare.com>

--- src/util.rs
+++ src/util.rs
@@ -9,4 +9,4 @@ impl<'a, 'b> ApplyToApp<'a, 'b> for App<'a, 'b> {
     }
 }
 
-pub const TEMP_NOTICE_ES_MODULES_DO_BETA: &str = "While Durable Objects are in Beta, ES Modules are limited to accounts which have opted-in to the Beta. You may do so by following the instructions here: https://developers.cloudflare.com/workers/learning/using-durable-objects";
+pub const TEMP_NOTICE_ES_MODULES_DO_BETA: &str = "Your account does not have permission to do this! While Durable Objects are in Beta, the modules format is limited to accounts which have opted-in to the Beta. You may do so by following the instructions here: https://developers.cloudflare.com/workers/learning/using-durable-objects";
