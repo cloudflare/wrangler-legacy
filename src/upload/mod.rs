ecf636ae8a6bb78844b1fb463ac0c09c7f77aaad
Taylor <tay333@gmail.com>
2021-07-02 14:13:42 -0700

    feat: add usage_model to metadata (#1961)
    and remove toggling usage model after upload
    this shouldn't be merged until the upload api supports usage_model in
    metadata
    
    Co-authored-by: Taylor Lee <tlee@cloudflare.com>

--- src/upload/mod.rs
+++ src/upload/mod.rs
@@ -40,25 +40,6 @@ pub fn script(
         anyhow::bail!(error_msg(res.text()?))
     }
 
-    if let Some(usage_model) = target.usage_model {
-        let addr = format!(
-            "https://api.cloudflare.com/client/v4/accounts/{}/workers/scripts/{}/usage-model",
-            target.account_id.load()?,
-            target.name,
-        );
-
-        let res = client
-            .put(&addr)
-            .json(&serde_json::json!({
-                "usage_model": usage_model.as_ref()
-            }))
-            .send()?;
-
-        if !res.status().is_success() {
-            anyhow::bail!(error_msg(res.text()?))
-        }
-    }
-
     Ok(())
 }
 
