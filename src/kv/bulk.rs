c097240e1449e5ebf41f017014460bca21feb19d
ocs <46542932+ocsfrank@users.noreply.github.com>
2021-09-09 09:32:01 -0700

    Allow the API endpoint to be configured via an environment variable (#2064)
    * add env var for endpoint url
    
    * add tests and update env var
    
    * simplify match statements
    
    * update env var naming

--- src/kv/bulk.rs
+++ src/kv/bulk.rs
@@ -8,9 +8,10 @@ use cloudflare::endpoints::workerskv::write_bulk::KeyValuePair;
 use cloudflare::endpoints::workerskv::write_bulk::WriteBulk;
 use cloudflare::framework::apiclient::ApiClient;
 use cloudflare::framework::auth::Credentials;
-use cloudflare::framework::{Environment, HttpApiClient, HttpApiClientConfig};
+use cloudflare::framework::{HttpApiClient, HttpApiClientConfig};
 
 use crate::commands::kv::format_error;
+use crate::http;
 use crate::http::feature::headers;
 use crate::settings::global_user::GlobalUser;
 use crate::settings::toml::Target;
@@ -29,11 +30,9 @@ fn bulk_api_client(user: &GlobalUser) -> Result<HttpApiClient> {
         default_headers: headers(None),
     };
 
-    HttpApiClient::new(
-        Credentials::from(user.to_owned()),
-        config,
-        Environment::Production,
-    )
+    let environment = http::get_environment()?;
+
+    HttpApiClient::new(Credentials::from(user.to_owned()), config, environment)
 }
 
 pub fn put(
