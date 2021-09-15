24cec39082a627ec416643d69027b8182a2a66dd
ocs <46542932+ocsfrank@users.noreply.github.com>
2021-09-14 17:00:40 -0700

    update to show an error message

--- src/commands/dev/edge/watch.rs
+++ src/commands/dev/edge/watch.rs
@@ -5,6 +5,7 @@ use crate::commands::dev::edge::setup;
 use crate::deploy::DeployTarget;
 use crate::settings::global_user::GlobalUser;
 use crate::settings::toml::Target;
+use crate::terminal::message::{Message, StdOut};
 use crate::watch::watch_and_build;
 
 use anyhow::Result;
@@ -37,11 +38,30 @@ pub fn watch_for_changes(
         // this allows the server to route subsequent requests
         // to the proper script
         let uploaded = setup::upload(&mut target, &deploy_target, &user, session_token, verbose);
-        if let Ok(token) = uploaded {
-            *preview_token = token;
-        } else {
-            refresh_session_channel.send(Some(()))?;
-            break;
+
+        match uploaded {
+            Ok(token) => {
+                *preview_token = token;
+            }
+            Err(err) => {
+                if let Some(err) = err.downcast_ref::<setup::BadRequestError>() {
+                    // if the API error code is 10049, then it's an expired preview token
+                    if err.0.contains("10049") {
+                        refresh_session_channel.send(Some(()))?;
+                        break;
+                    }
+
+                    // otherwise it is a non recoverable error
+                    StdOut::warn(&format!(
+                        "{}\nPlease terminate `wrangler dev` using Ctrl+C.",
+                        &err.0
+                    ));
+                } else {
+                    // For all other errors, we can retry refreshing. TODO: maybe find out all possible errors
+                    refresh_session_channel.send(Some(()))?;
+                    break;
+                }
+            }
         }
     }
 
