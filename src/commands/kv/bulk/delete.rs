8f792aabed665da96b99479af85633440fa7fc4c
David Tolnay <dtolnay@gmail.com>
2021-11-03 08:21:43 -0700

    Hide '{' in error message from formatting machinery
    This code was doing two inconsistent things with the bail macro args:
    the one in delete.rs used single `{` assuming the string would *not* get
    put into std::fmt, while the one in put.rs used double `{{` assuming
    that std::fmt *would* get applied to it.
    
    This change closes the inconsistency and makes room for the bail macro
    to catch issues like this statically in a future version.

--- src/commands/kv/bulk/delete.rs
+++ src/commands/kv/bulk/delete.rs
@@ -44,7 +44,11 @@ pub fn run(
                 serde_json::from_str(&data);
             match keys_vec {
                 Ok(keys_vec) => keys_vec.iter().map(|kv| { kv.key.to_owned() }).collect(),
-                Err(_) => anyhow::bail!("Failed to decode JSON. Please make sure to follow the format, [{\"key\": \"test_key\", \"value\": \"test_value\"}, ...]")
+                Err(_) => {
+                    // Hide '{' in this error message from the formatting machinery in bail macro
+                    let msg = "Failed to decode JSON. Please make sure to follow the format, [{\"key\": \"test_key\", \"value\": \"test_value\"}, ...]";
+                    anyhow::bail!(msg);
+                }
             }
         }
         Ok(_) => anyhow::bail!("{} should be a JSON file, but is not", filename.display()),
