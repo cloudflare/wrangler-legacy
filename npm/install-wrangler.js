9a4b44e907af9a1b7e31307736bf3aff2b8b2f51
Jacob M-G Evans <jacobmgevans@gmail.com>
2022-09-30 12:52:16 -0500

    Add deprecation message on install of Wrangler Legacy

--- npm/install-wrangler.js
+++ npm/install-wrangler.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
 
 const { install } = require("./binary");
-install()
+
+const deprecationMessage = `
+                      ⛔   DEPRECATION   ⛔
+The version of wrangler you are using is now deprecated. 
+
+Please update to the latest version of wrangler to prevent critical errors.
+
+Run \`npm uninstall -g @cloudflare/wrangler && npm install -g wrangler\` to update to the latest version
+`;
+
+install();
+console.log(deprecationMessage);
